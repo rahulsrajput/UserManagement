7b199d7841dc052185d323dd9fd294e4d56ba02a
rahulsrajput <rahulsrajput29@gmail.com>
2024-04-02 11:19:29 +0530

    .

--- userManagement/settings.py
+++ userManagement/settings.py
@@ -156,4 +156,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://user-managementl.vercel.app/"]
